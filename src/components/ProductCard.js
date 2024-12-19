892c4880e2fadb42043da4a1739280879952ad7a
Alicio <alicio.valle242@gmail.com>
2024-12-18 22:54:38 -0500

    added card to Product Card Component

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,3 +1,13 @@
-export default function ProductCard(props) {
+import { Card, Button, Form, Row, Col } from "react-bootstrap";
 
+export default function ProductCard(props) {
+    const product = props.product;
+
+    <Card>
+        <Card.Body>
+            <Card.Body>
+                <Card.Title></Card.Title>
+            </Card.Body>
+        </Card.Body>
+    </Card>
 }
