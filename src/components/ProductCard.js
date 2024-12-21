759a2c77f1ab51c8e7a4ff348dc875ba3c71417b
Alicio <alicio.valle242@gmail.com>
2024-12-20 23:04:22 -0500

    added button and info props to product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,11 +2,15 @@ import { Card, Button, Form, Row, Col } from "react-bootstrap";
 
 export default function ProductCard({ product }) {
 
-    <Card>
-        <Card.Body>
+    return (
+        <Card>
             <Card.Body>
-                <Card.Title></Card.Title>
+                <Card.Body>
+                    <Card.Title>{product.title}</Card.Title>
+                    <Card.Text>{product.price}</Card.Text>
+                    <Button variant='primary'>Add to Card</Button>
+                </Card.Body>
             </Card.Body>
-        </Card.Body>
-    </Card>
+        </Card>
+    )
 }
