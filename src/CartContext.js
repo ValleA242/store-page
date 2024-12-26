eceaffbcdd84f7e2b143f087bc403ad94835ec30
Alicio <alicio.valle242@gmail.com>
2024-12-25 23:33:59 -0500

    added helper functions to cart context

--- src/CartContext.js
+++ src/CartContext.js
@@ -1,2 +1,13 @@
 import { createContext, useState } from "react";
 import { productsArray } from "./productStore";
+
+export const CartContext = createContext({
+    items: [], //holds items in array 
+    getProductQuantity: () => { }, //this function will get passed to provider
+    addOneToCart: () => { },
+    removeOneFromCart: () => { },
+    deleteFromCart: () => { },
+    getTotalCost: () => { }
+});
+
+export function CartProvider({ children })
