2581f8b0af30796a81605338c308f9b9fb381635
Alicio Valle <alicio.valle242@gmail.com>
2024-12-29 17:01:44 -0500

    created navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Button, Navbar, Container, Modal } from react - bootstrap
+
+export default function NavbarComponent() {
+    return (
+        <Navbar expand="sm">
+            <Navbar.Brand href="/">Ecommerce Store</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+                <Button>Cart 0 Items</Button>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
