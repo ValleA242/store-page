243323bb13faa1590c643a9cfa019149b2d29be4
Alicio Valle <alicio.valle242@gmail.com>
2024-12-30 17:04:31 -0500

    import bootstrap stylesheet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import NavbarComponent from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-
+      <NavbarComponent />
     </div>
   );
 }
